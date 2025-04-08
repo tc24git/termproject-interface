5399f54fa6f2831edc8107dadb4c33ad7cf230b9
tc24git <tc24felt@siena.edu>
2025-04-08 15:08:17 -0400

    started text interface

---
+++ TextInterface.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class TextInterface implements Runnable, ActionListener, ItemListener, ChangeListener{
+
+        public static String[] books;
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'run'");
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'stateChanged'");
+        }
+
+
+
+
+
+}
